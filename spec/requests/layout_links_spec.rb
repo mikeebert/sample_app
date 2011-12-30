0b68f7fe791ff0eef7cb1e55faa4269567515fcb
mikeebert <mike@mikeebert.net>
2011-12-30 17:13:00 -0600

    Completed signin/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -45,4 +45,33 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content =>"Sign up")
     response.should have_selector('a[href="/"]>img')
   end
+  
+  describe "layout links when not signed" do
+    it "should have a sign in link" do
+      visit root_path
+      response.should have_selector("a", href: signin_path, content: "Sign in")
+    end
+  end
+  
+  describe "when signed in" do
+    
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email, with:@user.email
+      fill_in :password, with:@user.password
+      click_button
+    end
+    
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", href: signout_path, content: "Sign out")
+    end
+    
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", href: user_path(@user), content: "Profile")
+    end
+    
+  end
 end
