f10685f01a57fd28975fece7a38cecdc0598da42
mikeebert <mike@mikeebert.net>
2012-01-04 11:19:33 -0600

    Micropost interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   
-  def authenticate
-    redirect_to signin_path unless signed_in?
-  end
-  
 end
