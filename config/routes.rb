4bce51fccb51c919cecaedc14e666ec648617add
mikeebert <mike@mikeebert.net>
2012-01-05 14:21:17 -0600

    Initial interface for following users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 SampleApp::Application.routes.draw do
 
-  resources :users
+  resources :users do 
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: "pages#home"
+
   match '/contact', :to => "pages#contact"
   match '/about', :to => "pages#about"
   match '/help', :to => "pages#help"
